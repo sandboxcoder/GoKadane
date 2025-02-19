8ee84d0ec367e7b00e21abce7e0ba1ab6e4302b4
Richard Osborne <rosborne255@yahoo.com>
2025-02-19 10:57:40 -0600

    Adding additional test case for MaxSubArray

--- core/algorithm_test.go
+++ core/algorithm_test.go
@@ -4,10 +4,19 @@ import (
 	"testing"
 )
 
-func TestMaximumSubArray(t *testing.T) {
+func TestMaximumSubArray_WithNegNumbers(t *testing.T) {
 	var arr = []int{-10, -5, -1}
 	result := MaximumSubArray(arr)
 	expected := -1
+	if result != expected {
+		t.Errorf("The result %d doesnt match what was expected %d", result, expected)
+	}
+}
+
+func TestMaximumSubArray_WithNegPosNumbers(t *testing.T) {
+	var arr = []int{3, -10, -5, 1}
+	result := MaximumSubArray(arr)
+	expected := 3
 	if result != expected {
 		t.Errorf("The result %d doesnt match what was expected %d", result, expected)
 	}
