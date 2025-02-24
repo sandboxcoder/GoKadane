b5bc39624517341e8b207f708439d332a5b06baa
Richard Osborne <rosborne255@yahoo.com>
2025-02-24 08:06:45 -0600

    Adding test for sphere collision
    * Adding a test to verify two non-intersecting spheres do not collide

--- core/sphere_test.go
+++ core/sphere_test.go
@@ -11,3 +11,13 @@ func TestSphereCollision(t *testing.T) {
 		t.Errorf("checkCollision() != expected, returned %+v instead of true", detectedCollision)
 	}
 }
+
+func TestSphereCollision_Seperated(t *testing.T) {
+	s1 := Sphere{Vector3{0, 0, 0}, 20}
+	s2 := Sphere{Vector3{100, 0, 0}, 20}
+	detectedCollision := checkCollision(s1, s2)
+	expected := false
+	if detectedCollision != expected {
+		t.Errorf("checkCollision() != expected, returned %+v instead of %+v", detectedCollision, expected)
+	}
+}
