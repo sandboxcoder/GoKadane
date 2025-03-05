c89631e3bd7230daee5dd3f2af1fa595e1d3b685
Richard Osborne <rosborne255@yahoo.com>
2025-03-05 08:26:00 -0600

    Adding tests for box min/max getters

--- core/box_test.go
+++ core/box_test.go
@@ -13,6 +13,26 @@ func TestOverlaps(t *testing.T) {
 	}
 }
 
+func TestMax(t *testing.T) {
+	center := Vector3{1, 1, 1}
+	b1 := Box{Center: center, Extent: Vector3{X: 5.0, Y: 5.0, Z: 5.0}}
+	max := b1.GetMax()
+	expected := Vector3{6, 6, 6}
+	if max != expected {
+		t.Errorf("GetMax() != expected, returned %+v instead of expected value %f", max, expected)
+	}
+}
+
+func TestMin(t *testing.T) {
+	center := Vector3{1, 1, 1}
+	b1 := Box{Center: center, Extent: Vector3{X: 5.0, Y: 5.0, Z: 5.0}}
+	result := b1.GetMin()
+	expected := Vector3{-4, -4, -4}
+	if result != expected {
+		t.Errorf("result != expected, returned %+v instead of expected value %f", result, expected)
+	}
+}
+
 func TestExtent(t *testing.T) {
 	center := Vector3{0, 0, 0}
 	b1 := Box{Center: center, Extent: Vector3{X: 5.0, Y: 5.0, Z: 5.0}}
