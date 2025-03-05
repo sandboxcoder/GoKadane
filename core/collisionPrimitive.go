8a98f389dc2ca658cd700c279c268c3a76636ac4
Richard Osborne <rosborne255@yahoo.com>
2025-03-05 11:30:04 -0600

    Updating collision primitive collision test
    * Use sphere vs sphere for the worse case scenario

--- core/collisionPrimitive.go
+++ core/collisionPrimitive.go
@@ -1,5 +1,9 @@
 package core
 
+import (
+	"fmt"
+)
+
 type BoundaryType int
 
 const (
@@ -26,9 +30,30 @@ func (a CollisionPrimitive) IsColliding(b CollisionPrimitive) bool {
 			ret = a.sphereOverlaps(b)
 		}
 	} else {
-		ret = a.sphereOverlaps(b)
+		// Converting a sphere into a box is a fairly decent approx so we'll use that method.
+		// Converting a box -> sphere would return an even bigger volume causing more false positives.
+		ret = a.boxOverlaps(b)
 	}
 	return ret
+}
+
+func (c CollisionPrimitive) GetBoundingBox() Box {
+	b := Box{c.Center, c.Extent}
+	return b
+}
+
+func (c CollisionPrimitive) GetSphere() Sphere {
+	var s Sphere
+	if c.BoundaryType == Bounds_BoundingBox {
+		box := Box{c.Center, c.Extent}
+		s = box.GetSphere()
+	} else if c.BoundaryType == Bounds_SPHERE {
+		s = Sphere{c.Center, c.Extent.X}
+	} else {
+		s = Sphere{c.Center, c.Extent.X}
+		fmt.Printf("Warn: Unknown primitive type %+v", c.BoundaryType)
+	}
+	return s
 }
 
 func (a CollisionPrimitive) boxOverlaps(b CollisionPrimitive) bool {
