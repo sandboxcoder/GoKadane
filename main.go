ffbcd849d5c333226130d83516eb59c898456373
Richard Osborne <rosborne255@yahoo.com>
2025-03-16 11:50:27 -0500

    Revert "Removing main.go"
    This reverts commit 37c973d4530c0b54f46d9987b2791d5e982d2941.

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"Kadane/core"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Simple algorithm test program")
+
+	newTriangle := core.Triangle{Base: 5, Height: 5}
+	fmt.Println("area:", newTriangle.Area())
+
+	upVector := core.Vector3{X: 0, Y: 1, Z: 0}
+	leftVector := core.Vector3{X: 1, Y: 0, Z: 0}
+	fmt.Println("Two orthogonal vectors:", upVector.Dot(leftVector))
+	fmt.Println("Magnitude of the up vector:", upVector.Mag())
+
+	testVec := core.Vector3{X: 0, Y: 2, Z: 0}
+	testVec.Normalize()
+	fmt.Println("Normalized vec ", testVec)
+
+	crossProduct := upVector.Cross(leftVector)
+	fmt.Println("Cross product (up X left) = back vector:", crossProduct)
+}
