89f3a3f0786f995eb7a232ff69fd986236d5d855
Richard Osborne <rosborne255@yahoo.com>
2025-03-17 09:13:11 -0500

    Adding World.DoTick() unit test

--- core/world_test.go
+++ core/world_test.go
@@ -7,13 +7,14 @@ import (
 // Creates a list of sample entities
 func getEntities() []Entity {
 	zeroPos := Vector3{0, 0, 0}
+	vel := Vector3{1, 0, 0}
 	sphere := Sphere{zeroPos, 5}
 	spherePrimitive := sphere.GetPrimitive()
-	obj1 := CreateEntity(zeroPos, spherePrimitive)
+	obj1 := CreateEntityWithVelocity(zeroPos, vel, spherePrimitive)
 
 	pos2 := Vector3{2, 0, 0}
 	sphere2 := Sphere{pos2, 5}
-	obj2 := CreateEntity(pos2, sphere2.GetPrimitive())
+	obj2 := CreateEntityWithVelocity(pos2, vel, sphere2.GetPrimitive())
 
 	list := []Entity{obj1, obj2}
 	return list
@@ -26,5 +27,18 @@ func TestWorld_AddEntity(t *testing.T) {
 	result := world.GetNumEntities()
 	if result != expected {
 		t.Errorf("result != expected, returned %+v instead of %+v", result, expected)
+	}
+}
+
+func TestWorld_DoTick(t *testing.T) {
+	list := getEntities()
+	world := World{list}
+	world.DoTick(1)
+	expected := Vector3{1, 0, 0}
+	for _, entity := range world.objects {
+		vel := entity.GetVelocity()
+		if vel.X != 1 {
+			t.Errorf("result != expected, returned %+v instead of %+v", vel, expected)
+		}
 	}
 }
