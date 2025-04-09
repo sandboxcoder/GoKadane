70dcb3dfa2f8ed4e80413a7a0e5be13d7321ecaa
Richard Osborne <rosborne255@yahoo.com>
2025-04-09 09:48:11 -0500

    Added addEntity() to check id if its valid

--- core/entityCollection.go
+++ core/entityCollection.go
@@ -6,21 +6,30 @@ type EntityCollection struct {
 	id      uint32 // Default is INVALID_ENTITY_ID
 }
 
+// Attempts to add the entity. Returns true if success
+func (collection *EntityCollection) addEntity(entity *Entity) bool {
+	const MaxUint = ^uint32(0)
+	var ret = false
+	if collection.id+1 <= MaxUint {
+		collection.id = collection.id + 1
+		entity.id = collection.id
+		collection.objects = append(collection.objects, *entity)
+		ret = true
+	}
+	return ret
+}
+
 func (collection *EntityCollection) AddEntity(pos Vector3, bound CollisionPrimitive) Entity {
 	entity := Entity{}
 	entity.position = pos
 	entity.CollisionPrimitive = bound
-	collection.id = collection.id + 1
-	entity.id = collection.id
-
-	collection.objects = append(collection.objects, entity)
+	collection.addEntity(&entity)
 	return entity
 }
 
 func (collection *EntityCollection) AddEntityWithVelocity(pos Vector3, vel Vector3, bound CollisionPrimitive) Entity {
-	collection.id = collection.id + 1
-	entity := Entity{pos, vel, bound, collection.id}
-	collection.objects = append(collection.objects, entity)
+	entity := Entity{pos, vel, bound, INVALID_ENTITY_ID}
+	collection.addEntity(&entity)
 	return entity
 }
 
