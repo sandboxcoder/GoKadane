125234d899eb299ed8ed38f453328885877711d2
Richard Osborne <rosborne255@yahoo.com>
2025-04-17 21:02:29 -0500

    Adding test to retrieve entity from a collection

--- core/entityCollection_test.go
+++ core/entityCollection_test.go
@@ -30,3 +30,17 @@ func TestEntityCollection_AddEntityWithVelocity(t *testing.T) {
 		t.Errorf("result != expected, velocity %+v", obj1.GetVelocity())
 	}
 }
+
+func TestEntityCollection_GetEntity(t *testing.T) {
+	zeroPos := Vector3{0, 0, 0}
+	sphere := Sphere{zeroPos, 5}
+	spherePrimitive := sphere.GetPrimitive()
+	collection := EntityCollection{}
+	obj1 := collection.AddEntity(zeroPos, spherePrimitive)
+	other := collection.Get(0)
+	result := obj1.GetId() == other.GetId()
+	expect := true
+	if result != expect {
+		t.Errorf("result != expected, returned %+v", result)
+	}
+}
