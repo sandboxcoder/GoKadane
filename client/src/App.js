5bc0dad929aabd07f2302e1d6b3eabf0ef9ccc4e
Richard Osborne <rosborne255@yahoo.com>
2025-05-11 09:44:26 -0500

    Add react-bootstrap
    use an example component as a placeholder

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Accordion from 'react-bootstrap/Accordion';
 
 function App() {
   const [message, setMessage] = useState('');
@@ -11,10 +12,26 @@ function App() {
   }, []);
 
   return (
-    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>React + Go</h1>
-      <p>{message}</p>
-    </div>
+    <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          {message}
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
   );
 }
 
